762f30a1073bc49f47f1b1e76e61259a6581c55a
Ivan Ramirez <ivanramirez@google.com>
2025-11-27 06:48:18 -0500

    fix(deploy): use public IP for cloud sql connection

--- src/secrets.js
+++ src/secrets.js
@@ -13,6 +13,8 @@ async function loadSecrets() {
     try {
         process.env.DB_USER = await accessSecretVersion('DB_USER');
         process.env.DB_PASSWORD = await accessSecretVersion('DB_PASSWORD');
+        process.env.DB_HOST = await accessSecretVersion('DB_HOST');
+        process.env.DB_PORT = await accessSecretVersion('DB_PORT');
         process.env.GOOGLE_CLIENT_ID = await accessSecretVersion('GOOGLE_CLIENT_ID');
         process.env.GOOGLE_CLIENT_SECRET = await accessSecretVersion('GOOGLE_CLIENT_SECRET');
         process.env.AUTHORIZED_USERS = await accessSecretVersion('AUTHORIZED_USERS');
