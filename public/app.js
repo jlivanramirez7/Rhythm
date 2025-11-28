c861bad61370c912eb6da867a132e0c9d0bf721e
Ivan Ramirez <ivanramirez@google.com>
2025-11-27 21:23:21 -0500

    fix: Await logOrUpdateReading to ensure UI updates

--- public/app.js
+++ public/app.js
@@ -388,7 +388,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 end_date: document.getElementById('end-date').value,
                 range: document.getElementById('range-checkbox').checked
             };
-            logOrUpdateReading(payload);
+            await logOrUpdateReading(payload);
         });
 
         periodButton.addEventListener('click', async () => {
